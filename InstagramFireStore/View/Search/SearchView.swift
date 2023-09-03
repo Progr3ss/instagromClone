0f3904ce6daabf0017a6e594df6f4c99d1ab04d6
Progr3ss <mchibwe01@gmail.com>
2023-09-03 15:35:37 -0700

    search bar

--- InstagramFireStore/View/Search/SearchView.swift
+++ InstagramFireStore/View/Search/SearchView.swift
@@ -8,8 +8,15 @@
 import SwiftUI
 
 struct SearchView: View {
+  @State var searchText = ""
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+      ScrollView {
+       SearchBar(text: $searchText)
+          .padding()
+        
+        //Search Bar
+        
+      }
     }
 }
 
