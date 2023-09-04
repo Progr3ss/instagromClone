8b9a0e55138586530f01cfb7efb6dc3d9e60213e
Progr3ss <mchibwe01@gmail.com>
2023-09-04 01:01:39 -0700

    setting up firebase

--- InstagramFireStore/InstagramFireStoreApp.swift
+++ InstagramFireStore/InstagramFireStoreApp.swift
@@ -6,9 +6,13 @@
 //
 
 import SwiftUI
+import FirebaseCore
 
 @main
 struct InstagramFireStoreApp: App {
+  init() {
+    FirebaseApp.configure()
+  }
     var body: some Scene {
         WindowGroup {
             LoginView()
