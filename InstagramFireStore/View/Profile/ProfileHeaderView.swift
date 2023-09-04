aec32ffe2ceb2ad2db5e76f5f90d451946b94ee1
Progr3ss <mchibwe01@gmail.com>
2023-09-03 18:10:08 -0700

    added ProfileHeaderView

---
+++ InstagramFireStore/View/Profile/ProfileHeaderView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileHeaderView.swift
+//  InstagramFireStore
+//
+//  Created by martin chibwe on 9/3/23.
+//
+
+import SwiftUI
+
+struct ProfileHeaderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileHeaderView()
+}
