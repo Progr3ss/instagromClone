688467589eaf164f0bc70b9a006b336dc784901b
Progr3ss <mchibwe01@gmail.com>
2023-09-03 22:08:48 -0700

    added image picker

---
+++ InstagramFireStore/UTils/ImagePicker.swift
@@ -0,0 +1,8 @@
+//
+//  ImagePicker.swift
+//  InstagramFireStore
+//
+//  Created by martin chibwe on 9/3/23.
+//
+
+import Foundation
--- InstagramFireStore/View/UploadPost/UploadPostView.swift
+++ InstagramFireStore/View/UploadPost/UploadPostView.swift
@@ -2,50 +2,14 @@
 //  UploadPostView.swift
 //  InstagramFireStore
 //
-//  Created by martin chibwe on 9/2/23.
+//  Created by martin chibwe on 9/3/23.
 //
 
 import SwiftUI
 
 struct UploadPostView: View {
-  @State private var selectedImage: UIImage?
-  @State var postImage: Image?
-  @State var captionText = ""
     var body: some View {
-      if postImage == nil {
-        Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
-          Image("plus_photo")
-            .resizable()
-            .renderingMode(.template)
-            .scaledToFill()
-            .frame(width: 180, height: 180)
-            .clipped()
-            .padding(.top,56)
-            .foregroundColor(.black)
-        })
-      } else {
-        HStack(alignment:.top) {
-          Image("image5")
-            .resizable()
-            .scaledToFill()
-            .frame(width: 96, height: 96)
-            .clipped()
-            
-          TextField("Enter your cpation", text: $captionText)
-           
-        }
-        Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
-          Text("Share")
-            .font(.system(size: 16, weight: .semibold))
-            .frame(width: 360, height: 50)
-            .background(.blue)
-            .foregroundColor(.white)
-            .cornerRadius(5)
-          
-        }).padding()
-        
-      }
-      Spacer()
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
